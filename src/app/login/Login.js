27aec3da7cdaf3431f35667634de225bb502c824
William Nunes <williamfenu@github.com>
2020-01-11 20:41:18 -0300

    estilização do login

--- src/app/login/Login.js
+++ src/app/login/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import useForm from "react-hook-form";
 import rest from "../../commons/service/rest";
+import "./styles.css";
 
 const loginRest = rest("login");
 
@@ -25,31 +26,43 @@ const Login = props => {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <label htmlFor="username">nome</label>
-        <input
-          name="username"
-          autoComplete="username"
-          id="username"
-          ref={register({ required: true })}
-          onChange={e => setLogin({ ...login, username: e.target.value })}
-        ></input>
-        <label htmlFor="password">Senha</label>
-        <input
-          name="password"
-          autoComplete="current-password"
-          type="password"
-          id="password"
-          ref={register({ required: true })}
-          onChange={e => setLogin({ ...login, password: e.target.value })}
-        ></input>
-        <button onClick={handleSubmit(handleLogin)}>Entrar</button>
-      </form>
-      <span hidden={!invalidPassword} style={{ color: "red" }}>
-        Usuário ou senha inválidos
-      </span>
+    <div className="login-container">
+      <div className="login-box">
+        <h1 className="header">Login</h1>
+        <hr />
+        <form className="form container">
+          <label className="label" htmlFor="username">
+            Nome:
+          </label>
+          <input
+            name="username"
+            autoComplete="username"
+            id="username"
+            ref={register({ required: true })}
+            onChange={e => setLogin({ ...login, username: e.target.value })}
+          ></input>
+          <label className="label" htmlFor="password">
+            Senha:
+          </label>
+          <input
+            name="password"
+            autoComplete="current-password"
+            type="password"
+            id="password"
+            ref={register({ required: true })}
+            onChange={e => setLogin({ ...login, password: e.target.value })}
+          ></input>
+          <button
+            className="btn btn-primary button"
+            onClick={handleSubmit(handleLogin)}
+          >
+            Entrar
+          </button>
+        </form>
+        <span hidden={!invalidPassword} style={{ color: "red" }}>
+          Usuário ou senha inválidos
+        </span>
+      </div>
     </div>
   );
 };
